2f44469879cdf47743fa51cacec5fc102e81e06f
Mikey Dodson <mikeyleedodson@gmail.com>
2025-11-11 10:51:36 +0000

    Image Slider Upgrade

--- Project/js/imageSlider.js
+++ Project/js/imageSlider.js
@@ -1,23 +1,35 @@
-let images = ["images/cantorbuildingoutside.jpg","images/StayBright.jpg"]
+let images = ["images/cantorbuildingoutside.jpg","images/StayBright.jpg","images/TallBuilding.jpg"]
+let altTags = ["The outside of Cantor College","A building that says \"Stay Bright\" on the side","Tall red brick building in the sun"]
 
 let imgSlider = document.getElementById("imgSlider")
 console.info(imgSlider)
 
 let imageIndex = 0
 
+let hover = false
+
 function slide()
 {
-    imageIndex +=1
-    if (imageIndex> images.length-1)
+    if (hover)
     {
-        imageIndex = 0
+        imageIndex +=1
+        if (imageIndex> images.length-1)
+        {
+            imageIndex = 0
+        }
+        imgSlider.src = images[imageIndex]
+        imgSlider.alt = altTags[imageIndex]
     }
-    imgSlider.src = images[imageIndex]
+
 }
-
 imgSlider.addEventListener('click',function(ev){
-
-
-    slide();
+    slide()
+})
+imgSlider.addEventListener('mouseover',function(ev){
+    hover = true
+})
+imgSlider.addEventListener('mouseleave',function(ev){
+    hover = false
 })
 
+setInterval(slide,2000)
